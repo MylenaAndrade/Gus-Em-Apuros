bd642db437c98a06c40c69d57f18e0100a652b96
Mylena Andrade <mylenaandrade200@gmail.com>
2025-02-24 19:38:35 -0300

    feat: mudando o livro de hierarquia

--- Assets/Scripts/Desafios/Biblioteca/Estante.cs
+++ Assets/Scripts/Desafios/Biblioteca/Estante.cs
@@ -5,5 +5,18 @@ using UnityEngine;
 
 public class Estante : MonoBehaviour
 {
+    private void OnTriggerEnter2D(Collider2D livroFlutuante)
+    {
+        Transform livroNaEstante = transform.GetChild(transform.childCount - 1);
+
+        Livro livro = livroFlutuante.GetComponent<Livro>();
+        livro.MudandoDeEstante(livroNaEstante.gameObject, livroFlutuante.gameObject);
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log($"Objeto saiu da área: {other.gameObject.name}");
+    }
 
 }
--- Assets/Scripts/Desafios/Biblioteca/Livro.cs
+++ Assets/Scripts/Desafios/Biblioteca/Livro.cs
@@ -7,35 +7,50 @@ public class Livro : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHan
 {
     private Vector3 originalPosition;  // Guarda a posição inicial do livro
     private Transform originalShelf;   // Guarda a prateleira original
+
+    private Transform referencia; // Referência para a posição da prateleira
     
-    private Estante estante; // Referência à prateleira
+    private Transform estantePai; // Referência à prateleira
+
+    private static Livro _instancia;
+
 
     private void Start()
     {
+        gameObject.GetComponent<Collider2D>().enabled = false; // Desabilita o collider
+        SetEstantePai(transform.parent);     // Salva a prateleira original
+        originalPosition = transform.position; // Salva a posição inicial do livro
         PodeSerArrastado(); // Se não for o último, bloqueia o arrasto
     }
 
+
     public void OnBeginDrag(PointerEventData eventData)
     {
-       
-        originalPosition = transform.position; // Salva a posição inicial do livro
-        originalShelf = transform.parent;     // Salva a prateleira original
+        gameObject.GetComponent<Collider2D>().enabled = true; // Habilita o collider
         transform.SetParent(transform.root);  // Remove da prateleira temporariamente
     }
 
     public void OnDrag(PointerEventData eventData)
     {
+        
         transform.position = eventData.position; // Segue o cursor enquanto arrasta
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        ReturnToOriginalPosition();  // Se não soltou em uma prateleira, volta ao local original
+        gameObject.GetComponent<Collider2D>().enabled = false; // Desabilita o collider
+        
+        if(transform.parent == transform.root) // Se não foi solto em uma prateleira
+        {
+            ReturnToOriginalPosition(); // Retorna à posição original
+            return;
+        } 
+        
     }
 
     private void ReturnToOriginalPosition()
     {
-        transform.SetParent(originalShelf);
+        transform.SetParent(GetEstantePai());
         transform.position = originalPosition; // Retorna à posição original
     }
 
@@ -50,5 +65,41 @@ public class Livro : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHan
             GetComponent<CanvasGroup>().blocksRaycasts = false; // Se não for o último, bloqueia o arrasto
         }
     }
+
+    public Transform GetEstantePai()
+    {
+        return estantePai;
+    }
+
+    public void SetEstantePai(Transform estante)
+    {
+        estantePai = estante;
+    }
+
+    public void MudandoDeEstante(GameObject gameObjectLivroEstante, GameObject gameObjectFlutuante)
+    {
+        Debug.Log($"O 1 livro é {gameObjectFlutuante.name}");
+        Debug.Log($"O livro é {gameObjectLivroEstante.name}");
+        if(gameObjectFlutuante.name == gameObjectLivroEstante.name)
+        {
+            gameObjectFlutuante.transform.SetParent(gameObjectLivroEstante.transform.parent);
+          //  Vector3 novaPosicao = gameObjectLivroEstante.transform.TransformPoint(new Vector3(2, 0, 0));
+          //  gameObjectFlutuante.transform.position = novaPosicao;
+            Debug.Log($"Filho: {gameObjectFlutuante.transform.parent.name}");
+
+            PodeSerArrastado();
+            Debug.Log("Os livros são iguais");
+            
+        }
+        else
+        {
+            Debug.Log("Os livros são diferentes");
+        }
+    }
+    
+    
+    
+
+    
     
 }
